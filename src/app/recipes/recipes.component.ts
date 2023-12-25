9be0de892b46f5172a9e2792069f50354e4e20a4
satishsaraskar <satishsaraskar@gmail.com>
2023-12-25 17:47:01 +0530

    Template Driven form impliment & add validation amount -1 not save in amount

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -10,17 +10,17 @@ import { RecipeService } from './recipe.service';
 })
 export class RecipesComponent implements OnInit   {
    
-  selectedRecipe!: Recipe;
+  // selectedRecipe!: Recipe;
   constructor(private recipeService :RecipeService){}
 
   ngOnInit() {
 
-    this.recipeService.recipeSelected
-    .subscribe(
-      (recipe:Recipe)=>{
-        this.selectedRecipe = recipe ; 
-      }
-    )
+    // this.recipeService.recipeSelected
+    // .subscribe(
+    //   (recipe:Recipe)=>{
+    //     this.selectedRecipe = recipe ; 
+    //   }
+    // )
   }
    
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,11 @@
+// import { Ingredient } from '../shared/ingredient.model';
+// import { EventEmitter } from '@angular/core';
+import { Subject } from 'rxjs';
 import { Ingredient } from '../shared/ingredient.model';
-import { EventEmitter } from '@angular/core';
 
 export class ShoppingListService {
-  ingredientsChanged = new EventEmitter<Ingredient[]>();  //add Ingredients using origin array 
-
+ // ingredientsChanged = new EventEmitter<Ingredient[]>();  //add Ingredients using origin array 
+ ingredientsChanged = new Subject<Ingredient[]>();  //add Ingredients using origin array 
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
@@ -15,7 +17,7 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 
   addIngredients(ingredients: Ingredient[]) {
@@ -23,7 +25,7 @@ export class ShoppingListService {
     //   this.addIngredient(ingredient);
     // }
     this.ingredients.push(...ingredients);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
  
 
